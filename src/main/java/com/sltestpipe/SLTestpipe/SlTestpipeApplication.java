3fb873dcba9f4cbb87affa50bfbe4fd555ab1229
student <abhiewitec@gmail.com>
2021-02-27 17:09:22 +0000

    pipeline using SCM

---
+++ src/main/java/com/sltestpipe/SLTestpipe/SlTestpipeApplication.java
@@ -0,0 +1,13 @@
+package com.sltestpipe.SLTestpipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlTestpipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlTestpipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sltestpipe/SLTestpipe/SlTestpipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sltestpipe.SLTestpipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlTestpipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
